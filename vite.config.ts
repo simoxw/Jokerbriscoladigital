be75a5c4fe399efe2bd5c83c91319b6daceab7ea
simoxw <enomisrasalgul@gmail.com>
2026-02-02 18:32:18 +0100

    Deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,17 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Jokerbriscoladigital/',
   server: {
     proxy: {
       '/socket.io': {
         target: 'http://127.0.0.1:3000',
         ws: true,
         changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
-});
+        secure: false
+      }
+    }
+  }
+})
