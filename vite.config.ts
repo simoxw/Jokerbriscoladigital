a02f36b9dcb710ece8723f1509a3cb60abdf0167
simoxw <enomisrasalgul@gmail.com>
2026-02-02 19:02:25 +0100

    depl

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/Jokerbriscoladigital/',
-  server: {
-    proxy: {
-      '/socket.io': {
-        target: 'http://127.0.0.1:3000',
-        ws: true,
-        changeOrigin: true,
-        secure: false
-      }
-    }
-  }
 })
